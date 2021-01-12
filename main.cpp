d6360e8bcdb6e215bd5f10f995d7ae168c835cad
Repumba <75318192+Repumba@users.noreply.github.com>
2021-01-12 11:50:37 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 
 #include "cli.h"
 #include "mainAlgorithm.h"
+#include "MeasureTime.h"
 
 
 int main() {
 
     UserInterface * cli = new UserInterface();
+    MeasureTime * mt = new MeasureTime();
+
     cli->acquireData();
+    mt->startClock();
     
     Solver * solver = new Solver(
         cli->getWorkers(),
@@ -20,7 +24,12 @@ int main() {
     cli->setFeedingTimestamps(solver->getFeedingTimestamps());
     cli->setTotalTimeWait(solver->getTotalWaitingTime());
 
+    mt->stopClock();
     cli->printTotalTimeWait();
     cli->printFeedingTimestamps();
+
+    std::cout << "Total time [ms]:" << mt->getTimeMilli() << std::endl;
+    std::cout << "Total time [us]:" << mt->getTimeMicro() << std::endl;
+    std::cout << "Total time [ns]:" << mt->getTimeNano() << std::endl;
 
 }
